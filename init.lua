67e2a29a5b27fd060bdec30b167198eb43bfb928
JuanScerriE <juan.scerri.js@gmail.com>
2022-01-14 23:45:22 +0100

    add some comments

--- init.lua
+++ init.lua
@@ -7,14 +7,18 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
+-- plugins
 require("plugs")
 
+-- options
 require("opts")
 require("plug-opts")
 
+-- lsp & auto-complete
 require("lsp")
 require("aucmp")
 
+-- bindings
 require("binds")
 require("plug-binds")
 require("lsp-binds")
