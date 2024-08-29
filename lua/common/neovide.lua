19e8a6e5e72ba202fe8378b92e7099219cd8ac72
Juan Scerri <juan.scerri.js@gmail.com>
2024-08-29 17:04:11 +0200

    this is a huge overall using kickstart as the base

--- lua/common/neovide.lua
+++
@@ -1,4 +0,0 @@
-vim.o.guifont = "Iosevka Term:h16" -- text below applies for VimScript
-vim.g.neovide_cursor_animation_length = 0.1
-vim.g.neovide_cursor_trail_size = 0.1
-vim.g.neovide_cursor_animate_command_line = false
