f72bac98c124c4c1de69d4ba7745d192ff6c1296
Juan Scerri <juan.scerri.js@gmail.com>
2025-01-31 15:03:03 +0100

    remove some useless comments

--- init.lua
+++ init.lua
@@ -40,31 +40,22 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("n", "<leader>O", "<cmd>set spell!<cr>", { desc = "Toggle [O]rthographic Checking" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- TIP: Disable arrow keys in normal mode
+vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "[W]indow" })
+vim.keymap.set("n", "<leader>mo", "<C-o>", { desc = "Ju[m]p to [O]ld position" })
+vim.keymap.set("n", "<leader>mn", "<C-i>", { desc = "Ju[m]p to [N]ew position" })
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
-vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "[W]indow" })
-vim.keymap.set("n", "<leader>mo", "<C-o>", { desc = "Ju[m]p to [O]ld position" })
-vim.keymap.set("n", "<leader>mn", "<C-i>", { desc = "Ju[m]p to [N]ew position" })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	desc = "highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("juan-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 })
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -72,138 +63,88 @@ if not vim.loop.fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		error("Error cloning lazy.nvim:\n" .. out)
 	end
-end ---@diagnostic disable-next-line: undefined-field
+end
+
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({
-	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	"tpope/vim-fugitive", -- Add support for Git
+	{ "tpope/vim-sleuth" }, -- detect tabstop and shiftwidth automatically
 
-	-- write nicely typeset math in neovim (tex/latex integration)
-	-- {
-	-- 	"lervag/vimtex",
-	-- 	ft = "tex",
-	-- 	init = function()
-	-- 		-- we want to use treesitter
-	-- 		vim.g.vimtex_syntax_enabled = 0
-	-- 		vim.g.vimtex_syntax_conceal_disable = 1
-	--
-	-- 		vim.g.vimtex_compiler_latexmk_engines = {
-	-- 			["_"] = "-lualatex -shell-escape",
-	-- 		}
-	-- 	end,
-	-- 	config = function()
-	-- 		vim.opt.cole = 0
-	--
-	-- 		if vim.uv.os_uname().sysname == "Darwin" then
-	-- 			vim.g.vimtex_view_method = "skim"
-	-- 		end
-	-- 	end,
-	-- },
+	{ "tpope/vim-fugitive" }, -- add support for Git
 
-	-- Here is a more advanced example where we pass configuration
-	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-	--    require('gitsigns').setup({ ... })
-	--
-	-- See `:help gitsigns` to understand what the configuration keys do
-	-- { -- Adds git related signs to the gutter, as well as utilities for managing changes
-	-- 	"lewis6991/gitsigns.nvim",
-	-- 	opts = {
-	-- 		signs = {
-	-- 			add = { text = "+" },
-	-- 			change = { text = "~" },
-	-- 			delete = { text = "_" },
-	-- 			topdelete = { text = "‾" },
-	-- 			changedelete = { text = "~" },
-	-- 		},
-	-- 	},
-	-- },
-
-	{ -- Useful plugin to show you pending keybinds.
+	{ -- useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
-		-- event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		opts = {
 			icons = {
 				mappings = false,
 			},
 		},
+		lazy = false,
 	},
 
-	-- NOTE: Plugins can specify dependencies.
-	--
-	-- The dependencies are proper plugin specifications as well - anything
-	-- you do for a plugin at the top level, you can do for a dependency.
-	--
-	-- Use the `dependencies` key to specify the dependencies of a particular plugin
+	{ -- improve the navigation experience
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup()
+			-- mimic vim-vinegar
+			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open Parent Directory" })
+		end,
+		lazy = false,
+	},
 
-	{ -- Fuzzy Finder (files, lsp, etc)
+	{ -- autoformat
+		"stevearc/conform.nvim",
+		keys = {
+			{
+				"<leader>F",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform can also run multiple formatters sequentially
+				python = { "ruff_organize_imports", "ruff_format" },
+				ocaml = { "ocamlformat" },
+				tex = { "latexindent" },
+				--
+				-- You can use a sub-list to tell conform to run *until* a formatter
+				-- is found.
+				-- javascript = { { "prettierd", "prettier" } },
+			},
+			formatters = {
+				latexindent = {
+					prepend_args = { "-l", ".tex-format.yaml" },
+				},
+			},
+		},
+		lazy = false,
+	},
+
+	{ -- fuzzy Finder (files, lsp, etc)
 		"nvim-telescope/telescope.nvim",
-		-- event = "VimEnter",
 		branch = "0.1.x",
 		dependencies = {
-			"nvim-lua/plenary.nvim",
+			{ "nvim-lua/plenary.nvim" },
+
 			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 				"nvim-telescope/telescope-fzf-native.nvim",
-
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
 				build = "make",
-
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
 				cond = function()
 					return vim.fn.executable("make") == 1
 				end,
 			},
-			{ "nvim-telescope/telescope-ui-select.nvim" },
 
-			-- Useful for getting pretty icons, but requires a Nerd Font.
-			-- { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+			{ "nvim-telescope/telescope-ui-select.nvim" },
 		},
 		config = function()
-			-- Telescope is a fuzzy finder that comes with a lot of different things that
-			-- it can fuzzy find! It's more than just a "file finder", it can search
-			-- many different aspects of Neovim, your workspace, LSP, and more!
-			--
-			-- The easiest way to use Telescope, is to start by doing something like:
-			--  :Telescope help_tags
-			--
-			-- After running this command, a window will open up and you're able to
-			-- type in the prompt window. You'll see a list of `help_tags` options and
-			-- a corresponding preview of the help.
-			--
-			-- Two important keymaps to use while in Telescope are:
-			--  - Insert mode: <c-/>
-			--  - Normal mode: ?
-			--
-			-- This opens a window that shows you all of the keymaps for the current
-			-- Telescope picker. This is really useful to discover what Telescope can
-			-- do as well as how to actually do it!
-
-			-- [[ Configure Telescope ]]
-			-- See `:help telescope` and `:help telescope.setup()`
 			require("telescope").setup({
-				-- You can put your default mappings / updates / etc. in here
-				--  All the info you're looking for is in `:help telescope.setup()`
-				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
-				-- pickers = {}
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
@@ -252,6 +193,53 @@ require("lazy").setup({
 			end, { desc = "[F]ind [N]eovim files" })
 		end,
 	},
+
+	-- write nicely typeset math in neovim (tex/latex integration)
+	-- {
+	-- 	"lervag/vimtex",
+	-- 	ft = "tex",
+	-- 	init = function()
+	-- 		-- we want to use treesitter
+	-- 		vim.g.vimtex_syntax_enabled = 0
+	-- 		vim.g.vimtex_syntax_conceal_disable = 1
+	--
+	-- 		vim.g.vimtex_compiler_latexmk_engines = {
+	-- 			["_"] = "-lualatex -shell-escape",
+	-- 		}
+	-- 	end,
+	-- 	config = function()
+	-- 		vim.opt.cole = 0
+	--
+	-- 		if vim.uv.os_uname().sysname == "Darwin" then
+	-- 			vim.g.vimtex_view_method = "skim"
+	-- 		end
+	-- 	end,
+	-- },
+
+	-- Here is a more advanced example where we pass configuration
+	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+	--    require('gitsigns').setup({ ... })
+	--
+	-- See `:help gitsigns` to understand what the configuration keys do
+	-- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+	-- 	"lewis6991/gitsigns.nvim",
+	-- 	opts = {
+	-- 		signs = {
+	-- 			add = { text = "+" },
+	-- 			change = { text = "~" },
+	-- 			delete = { text = "_" },
+	-- 			topdelete = { text = "‾" },
+	-- 			changedelete = { text = "~" },
+	-- 		},
+	-- 	},
+	-- },
+
+	-- NOTE: Plugins can specify dependencies.
+	--
+	-- The dependencies are proper plugin specifications as well - anything
+	-- you do for a plugin at the top level, you can do for a dependency.
+	--
+	-- Use the `dependencies` key to specify the dependencies of a particular plugin
 
 	-- { -- LSP Configuration & Plugins
 	-- 	"neovim/nvim-lspconfig",
@@ -479,40 +467,6 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 
-	{ -- Autoformat
-		"stevearc/conform.nvim",
-		keys = {
-			{
-				"<leader>F",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				python = { "ruff_organize_imports", "ruff_format" },
-				ocaml = { "ocamlformat" },
-				tex = { "latexindent" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
-			},
-			formatters = {
-				latexindent = {
-					prepend_args = { "-l", ".tex-format.yaml" },
-				},
-			},
-		},
-		lazy = false,
-	},
-
 	-- { "micangl/cmp-vimtex", ft = "tex" },
 
 	-- { -- Autocompletion
@@ -646,16 +600,6 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 
-	{ -- improve the navigation experience
-		"stevearc/oil.nvim",
-		config = function()
-			require("oil").setup()
-			-- mimic vim-vinegar
-			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open Parent Directory" })
-		end,
-		lazy = false,
-	},
-
 	-- { -- Highlight, edit, and navigate code
 	-- 	"nvim-treesitter/nvim-treesitter",
 	-- 	build = ":TSUpdate",
@@ -711,24 +655,7 @@ require("lazy").setup({
 	-- { import = 'custom.plugins' },
 }, {
 	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
 		icons = {},
-		-- 	icons = vim.g.have_nerd_font and {} or {
-		-- 		cmd = "⌘",
-		-- 		config = "🛠",
-		-- 		event = "📅",
-		-- 		ft = "📂",
-		-- 		init = "⚙",
-		-- 		keys = "🗝",
-		-- 		plugin = "🔌",
-		-- 		runtime = "💻",
-		-- 		require = "🌙",
-		-- 		source = "📄",
-		-- 		start = "🚀",
-		-- 		task = "📌",
-		-- 		lazy = "💤 ",
-		-- 	},
 	},
 })
 
