f4ad265a2c09136b8f0e813217e4fd1e9801c0cf
Juan Scerri <juan.scerri.js@gmail.com>
2025-04-04 17:17:42 +0200

    add <leader>fm mapping for marks

--- init.lua
+++ init.lua
@@ -191,6 +191,7 @@ require("lazy").setup({
 
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "[F]ind [M]arks" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
