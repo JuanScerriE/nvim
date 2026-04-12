5f00b369772ef31fbb4d32328e76c281783bacf0
Juan Scerri <juan.scerri>
2026-04-12 11:08:41 +0200

    update

--- init.lua
+++ init.lua
@@ -402,7 +402,7 @@ require("lazy").setup({
 			}
 
 			vim.lsp.config["clangd"] = {
-				cmd = { "clangd", "--experimental-modules-support" },
+				cmd = { "clangd" },
 				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 				root_markers = {
 					"compile_commands.json",
