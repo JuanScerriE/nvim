415a4ea204c217470bbb591eab6379d2074b94a0
Juan Scerri <juan.scerri.js@gmail.com>
2026-07-14 22:25:38 +0200

    add vscode lsps

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.loader.enable()
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
+vim.g.editorconfig = true
 
 -- options
 vim.o.number = true
@@ -560,6 +561,27 @@ local servers = {
 					"xhtml",
 				},
 			},
+		},
+	},
+
+	html = {
+		cmd = function(dispatchers, config)
+			local cmd = "vscode-html-language-server"
+			if (config or {}).root_dir then
+				local local_cmd = vim.fs.joinpath(config.root_dir, "node_modules/.bin", cmd)
+				if vim.fn.executable(local_cmd) == 1 then
+					cmd = local_cmd
+				end
+			end
+			return vim.lsp.rpc.start({ cmd, "--stdio" }, dispatchers)
+		end,
+		filetypes = { "html" },
+		root_markers = { "package.json", ".git" },
+		settings = {},
+		init_options = {
+			provideFormatter = true,
+			embeddedLanguages = { css = true, javascript = true },
+			configurationSection = { "html", "css", "javascript" },
 		},
 	},
 }
