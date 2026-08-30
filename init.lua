7d20769c659f53ad0a3e5517ae3926101d4ea721
Juan Scerri <juan.scerri>
2026-08-30 10:39:17 +0200

    turn off virtual lines, causes too much flickering

--- init.lua
+++ init.lua
@@ -42,8 +42,8 @@ vim.diagnostic.config({
 	severity_sort = true,
 	float = { border = "rounded", source = "if_many" },
 	underline = { severity = { min = vim.diagnostic.severity.WARN } },
-	virtual_text = false,
-	virtual_lines = true,
+	virtual_text = true,
+	virtual_lines = false,
 	jump = {
 		on_jump = function(_, bufnr)
 			vim.diagnostic.open_float({
